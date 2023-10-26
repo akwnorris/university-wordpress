2c256a15615e99385129776ccdf3761dbcee4e8a
Alex Norris <akwnorris@gmail.com>
2023-10-26 11:29:26 -0500

    create function for metabox

--- app/public/wp-content/themes/fictional-university-theme/page.php
+++ app/public/wp-content/themes/fictional-university-theme/page.php
@@ -19,16 +19,15 @@ while(have_posts()) {
     <div class="container container--narrow page-section">
 
      <?php  
-      if (wp_get_post_parent_id(get_the_ID())) {
-        echo "I am a child page.";
-      }
-    ?>
-
-      <div class="metabox metabox--position-up metabox--with-home-link">
+      if (wp_get_post_parent_id(get_the_ID())) { ?>
+         <div class="metabox metabox--position-up metabox--with-home-link">
         <p>
           <a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main"><?php the_title() ?></span>
         </p>
       </div>
+        <?php  }
+    ?>
+
 
  <!--
       <div class="page-links">
