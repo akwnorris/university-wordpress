d2c313a8a7db547b93ac292816de565b0e7197a8
Alex Norris <akwnorris@gmail.com>
2023-10-27 11:20:19 -0500

    update event header and paragraph text

--- app/public/wp-content/themes/fictional-university-theme/archive-event.php
+++ app/public/wp-content/themes/fictional-university-theme/archive-event.php
@@ -5,9 +5,9 @@ get_header(); ?>
 <div class="page-banner">
 <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
 <div class="page-banner__content container container--narrow">
-  <h1 class="page-banner__title"><?php the_archive_title(); ?></h1>
+  <h1 class="page-banner__title">All Events</h1>
   <div class="page-banner__intro">
-    <p><?php the_archive_description(); ?></p>
+    <p>See what is going on in our world.</p>
   </div>
 </div>
 </div>
@@ -16,18 +16,7 @@ get_header(); ?>
   <?php 
     while(have_posts()) {
       the_post(); ?>
-      <div class="post-item">
-        <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-
-        <div class="metabox">
-          <p>Posted by <?php the_author_posts_link(); ?> on <?php the_time('n.j.y'); ?> in <?php echo get_the_category_list(', '); ?></p>
-        </div>
-
-        <div class="generic-content">
-          <?php the_excerpt(); ?>
-          <p><a class="btn btn--blue" href="<?php echo the_permalink(); ?>">Continue reading &raquo;</a></p>
-        </div>
-    </div>
+      
   <?php  }
 
       echo paginate_links();
