1a1540e81d0bc1543463a1103296df837b43b179
Alex Norris <akwnorris@gmail.com>
2023-10-27 11:03:54 -0500

    edit metabox for events

--- app/public/wp-content/themes/fictional-university-theme/single-event.php
+++ app/public/wp-content/themes/fictional-university-theme/single-event.php
@@ -17,7 +17,7 @@ while(have_posts()) {
 
     <div class="container container--narrow page-section">
     <div class="metabox metabox--position-up metabox--with-home-link">
-        <p><a class="metabox__blog-home-link" href="<?php echo site_url('/blog'); ?>"><i class="fa fa-home" aria-hidden="true"></i> Blog Home</a> Posted by <?php the_author_posts_link(); ?> on <?php the_time('n.j.y'); ?> in <?php echo get_the_category_list(', '); ?></p>
+        <p><a class="metabox__blog-home-link" href="<?php echo site_url('/blog'); ?>"><i class="fa fa-home" aria-hidden="true"></i>Event Home</a><?php the_title(); ?></p>
       </div>
 
         <div class="generic-content"><?php the_content(); ?>
