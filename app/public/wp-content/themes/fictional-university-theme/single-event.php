e31eb641dda863d806d48d0897908d669dace4d6
Alex Norris <akwnorris@gmail.com>
2023-10-27 11:11:21 -0500

    create array for new event slug

--- app/public/wp-content/themes/fictional-university-theme/single-event.php
+++ app/public/wp-content/themes/fictional-university-theme/single-event.php
@@ -17,7 +17,7 @@ while(have_posts()) {
 
     <div class="container container--narrow page-section">
     <div class="metabox metabox--position-up metabox--with-home-link">
-        <p><a class="metabox__blog-home-link" href="<?php echo site_url('/blog'); ?>"><i class="fa fa-home" aria-hidden="true"></i>Event Home</a><?php the_title(); ?></p>
+        <p><a class="metabox__blog-home-link" href="<?php echo site_url('/blog'); ?>"><i class="fa fa-home" aria-hidden="true"></i>Events Home</a> <span class="metabox_main"><?php the_title(); ?></span></p>
       </div>
 
         <div class="generic-content"><?php the_content(); ?>
