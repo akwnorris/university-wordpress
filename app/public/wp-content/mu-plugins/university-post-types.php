07269477018d7300e9870f82ce442dea28298a8e
Alex Norris <akwnorris@gmail.com>
2023-10-27 10:43:59 -0500

    add labels for event

--- app/public/wp-content/mu-plugins/university-post-types.php
+++ app/public/wp-content/mu-plugins/university-post-types.php
@@ -5,6 +5,10 @@ function university_post_types() {
         'public' => true,
         'labels' => array(
             'name' => 'Events',
+            'add_new_item' => 'Add New Event',
+            'edit_item' => 'Edit Event',
+            'all_items' => 'All Events',
+            'singular_name' => 'Event'
         ),
         'menu_icon' => 'dashicons-calendar'
     ));
