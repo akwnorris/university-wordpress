dc2ce60213bab006231edb91833f74d86ebbe92b
Alex Norris <akwnorris@gmail.com>
2023-10-27 18:10:24 -0500

    add image to professor page

--- app/public/wp-content/themes/fictional-university-theme/single-professor.php
+++ app/public/wp-content/themes/fictional-university-theme/single-professor.php
@@ -18,7 +18,7 @@ while(have_posts()) {
     <div class="container container--narrow page-section">
     
 
-        <div class="generic-content"><?php the_content(); ?>
+        <div class="generic-content"><?php the_post_thumbnail(); the_content(); ?>
         <?php
 
           $relatedPrograms = get_field('related_programs');
