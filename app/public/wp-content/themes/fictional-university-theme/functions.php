f1ae5e40a8147530e4c22db72c3c5bbd5eff9182
Alex Norris <akwnorris@gmail.com>
2023-10-27 10:34:55 -0500

    change events menu icon

--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -20,8 +20,9 @@ function university_post_types() {
     register_post_type('event', array(
         'public' => true,
         'labels' => array(
-            'name' => 'Events'
-        )
+            'name' => 'Events',
+        ),
+        'menu_icon' => 'dashicons-calendar'
     ));
 }
 
