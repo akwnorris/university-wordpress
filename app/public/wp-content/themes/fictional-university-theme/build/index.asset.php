7174721ea297baf54f6909146149f9984e84368d
Alex Norris <akwnorris@gmail.com>
2023-10-28 12:18:49 -0500

    create typing logic method

--- app/public/wp-content/themes/fictional-university-theme/build/index.asset.php
+++ app/public/wp-content/themes/fictional-university-theme/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery'), 'version' => '69f6568e3b17f1ee4c3d');
+<?php return array('dependencies' => array('jquery'), 'version' => '93f2e790ea9fc3fd0ea8');
--- app/public/wp-content/themes/fictional-university-theme/src/modules/Search.js
+++ app/public/wp-content/themes/fictional-university-theme/src/modules/Search.js
@@ -3,21 +3,27 @@ import $ from "jquery"
 class Search {
   // 1. describe and create/initiate our object
   constructor() {
-    this.openButton = $(".js-search-trigger")
-    this.closeButton = $(".search-overlay__close")
-    this.searchOverlay = $(".search-overlay")
-    this.isOverlayOpen = false
-    this.events()
+    this.openButton = $(".js-search-trigger");
+    this.closeButton = $(".search-overlay__close");
+    this.searchOverlay = $(".search-overlay");
+    this.searchField = $("#search-term");
+    this.events();
+    this.isOverlayOpen = false;
   }
 
   // 2. events
   events() {
-    this.openButton.on("click", this.openOverlay.bind(this))
-    this.closeButton.on("click", this.closeOverlay.bind(this))
-    $(document).on("keydown", this.keyPressDispatcher.bind(this))
+    this.openButton.on("click", this.openOverlay.bind(this));
+    this.closeButton.on("click", this.closeOverlay.bind(this));
+    $(document).on("keydown", this.keyPressDispatcher.bind(this));
+    this.searchField.on("keydown", this.typingLogic);
   }
 
   // 3. methods (function, action...)
+  typingLogic() {
+    alert("hello from typing logic");
+  }
+
   keyPressDispatcher(e) {
     if (e.keyCode == 83 && !this.isOverlayOpen) {
       this.openOverlay()
