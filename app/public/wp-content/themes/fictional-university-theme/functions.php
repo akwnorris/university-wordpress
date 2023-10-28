fd1619b3feaed82c58251f11f91b8c2bcd5d6f58
Alex Norris <akwnorris@gmail.com>
2023-10-28 10:11:43 -0500

    update map key

--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -31,7 +31,7 @@ function pageBanner($args = NULL) {
 <?php }
 
 function university_files() {
-    wp_enqueue_script('google-map', '//maps.googleapis.com/maps/api/js?key=AIzaSyBeetTiaOzapTlbj0iJ5B0ltPdMUf1eeys', NULL, '1.0', true);
+    wp_enqueue_script('google-map', '//maps.googleapis.com/maps/api/js?key=AIzaSyBGXf0h9keH35Q_IRoC2XjH-GVRXokVyh4', NULL, '1.0', true);
     wp_enqueue_script('main-university-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
     wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
     wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
