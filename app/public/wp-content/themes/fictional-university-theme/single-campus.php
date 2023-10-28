e81ae348616d918d499d65816d8b76429492114c
Alex Norris <akwnorris@gmail.com>
2023-10-28 10:34:57 -0500

    remove pin link on individual pages

--- app/public/wp-content/themes/fictional-university-theme/single-campus.php
+++ app/public/wp-content/themes/fictional-university-theme/single-campus.php
@@ -21,7 +21,7 @@ $mapLocation = get_field('map_location');
         <div class="acf-map">
  
        <div class="marker" data-lat="<?php echo $mapLocation['lat']; ?>" data-lng="<?php echo $mapLocation['lng']; ?>">
-       <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+       <h3><?php the_title(); ?></h3>
        <?php echo $mapLocation['address']; ?>
     </div>
   </div>
