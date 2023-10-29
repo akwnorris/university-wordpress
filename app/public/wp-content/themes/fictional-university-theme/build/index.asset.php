f86ba0852029a31487bb59e9677b208196544129
Alex Norris <akwnorris@gmail.com>
2023-10-28 22:02:19 -0500

    update search function results

--- app/public/wp-content/themes/fictional-university-theme/build/index.asset.php
+++ app/public/wp-content/themes/fictional-university-theme/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery'), 'version' => '01a17314f3351be07d18');
+<?php return array('dependencies' => array('jquery'), 'version' => 'f7af7e5b79a063e6e90a');
--- app/public/wp-content/themes/fictional-university-theme/src/modules/Search.js
+++ app/public/wp-content/themes/fictional-university-theme/src/modules/Search.js
@@ -46,8 +46,9 @@ class Search {
     $.getJSON('wp-json/wp/v2/posts?search=' + this.searchField.val(), posts => {
         this.resultsDiv.html(`
             <h2 class="search-overlay__section-title">General Information</h2>
-            <ul class="link-list min-list">
+            ${posts.length ? '<ul class="link-list min-list">' : '<p>No general information matches that search</p>'}
                 ${posts.map(item => `<li><a href="${item.link}">${item.title.rendered}</a></li>`).join('')}
+            ${posts.length ? '<ul>' : ''}
             </ul>
         `);
     });
