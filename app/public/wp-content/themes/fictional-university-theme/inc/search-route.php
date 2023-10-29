8126868861c91829415976575481c14d55e1832c
Alex Norris <akwnorris@gmail.com>
2023-10-29 14:21:41 -0500

    add arrays in search route for post type

--- app/public/wp-content/themes/fictional-university-theme/inc/search-route.php
+++ app/public/wp-content/themes/fictional-university-theme/inc/search-route.php
@@ -29,14 +29,16 @@ function universitySearchResults($data) {
     if (get_post_type() == 'post' OR get_post_type() == 'page') {
       array_push($results['generalInfo'], array(
         'title' => get_the_title(),
-        'permalink' => get_the_permalink()
+        'permalink' => get_the_permalink(),
+        'postType' => get_post_type(),
+        'authorName' => get_the_author()
       ));
     }
 
     if (get_post_type() == 'professor') {
       array_push($results['professors'], array(
         'title' => get_the_title(),
-        'permalink' => get_the_permalink()
+        'permalink' => get_the_permalink(),
       ));
     }
 
