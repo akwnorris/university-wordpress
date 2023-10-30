c81a4ef769dd73d6ed9602e4382d0f85c43edb87
Alex Norris <akwnorris@gmail.com>
2023-10-30 12:43:05 -0500

    pull content in on students notes page

--- app/public/wp-content/themes/fictional-university-theme/page-my-notes.php
+++ app/public/wp-content/themes/fictional-university-theme/page-my-notes.php
@@ -14,7 +14,25 @@ while(have_posts()) {
     ?>
 
     <div class="container container--narrow page-section">
-        test    
+        <ul class="min-list link-list" id="my-notes">
+            <?php
+                $userNotes = new WP_Query(array(
+                    'post_type' => 'note',
+                    'post_per_page' => -1,
+                    'author' => get_current_user_id()
+                ));
+            
+                while($userNotes->have_posts()) {
+                    $userNotes->the_post(); ?>
+                    <li>
+                        <input class="note-title-field" value="<?php echo esc_attr(get_the_title()); ?>">
+                        <textarea class="note-body-field"><?php echo esc_attr(get_the_content()); ?></textarea>
+                    </li>
+                    <?php
+                }
+            ?>
+
+        </ul>    
     </div>
     
 <?php }
