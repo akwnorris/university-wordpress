a31391cf87be54ba395de9cbc547a14706bc8eb6
Alex Norris <akwnorris@gmail.com>
2023-10-29 22:21:14 -0500

    remove admin bar for subscribers

--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -112,4 +112,14 @@ function redirectSubsToFrontend() {
   }
 }
 
+add_action('wp_loaded', 'noSubsAdminBar');
+
+function noSubsAdminBar() {
+  $ourCurrentUser = wp_get_current_user();
+
+  if (count($ourCurrentUser->roles) == 1 AND $ourCurrentUser->roles[0] == 'subscriber') {
+    show_admin_bar(false);
+  }
+}
+
 ?>
