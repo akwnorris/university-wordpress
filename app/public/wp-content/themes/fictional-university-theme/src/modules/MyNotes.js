e352b1189f93b20c54add1aa21148798d1ddba17
Alex Norris <akwnorris@gmail.com>
2023-10-30 13:26:17 -0500

    create save button on notes

--- app/public/wp-content/themes/fictional-university-theme/src/modules/MyNotes.js
+++ app/public/wp-content/themes/fictional-university-theme/src/modules/MyNotes.js
@@ -14,6 +14,7 @@ class MyNotes {
     editNote(e) {
         var thisNote = $(e.target).parents("li");
         thisNote.find(".note-title-field, .note-body-field").removeAttr("readonly").addClass("note-active-field");
+        thisNote.find(".update-note").addClass("update-note--visible");
     }
 
     deleteNote(e) {
