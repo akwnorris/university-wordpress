4fcd4e02a6cc791fcfd13ba07f5c9fc939c40011
Alex Norris <akwnorris@gmail.com>
2023-10-30 13:56:52 -0500

    make notes published on notes page

--- app/public/wp-content/themes/fictional-university-theme/src/modules/MyNotes.js
+++ app/public/wp-content/themes/fictional-university-theme/src/modules/MyNotes.js
@@ -6,9 +6,10 @@ class MyNotes {
   }
 
   events() {
-    $(".delete-note").on("click", this.deleteNote)
-    $(".edit-note").on("click", this.editNote.bind(this))
-    $(".update-note").on("click", this.updateNote.bind(this))
+    $(".delete-note").on("click", this.deleteNote);
+    $(".edit-note").on("click", this.editNote.bind(this));
+    $(".update-note").on("click", this.updateNote.bind(this));
+    $(".submit-note").on("click", this.createNote.bind(this));
   }
 
   // Methods will go here
@@ -68,7 +69,7 @@ class MyNotes {
       beforeSend: xhr => {
         xhr.setRequestHeader("X-WP-Nonce", universityData.nonce)
       },
-      url: universityData.root_url + "/wp-json/wp/v2/note/" + thisNote.data("id"),
+      url: universityData.root_url + '/wp-json/wp/v2/note/' + thisNote.data("id"),
       type: "POST",
       data: ourUpdatedPost,
       success: response => {
@@ -79,6 +80,33 @@ class MyNotes {
       error: response => {
         console.log("Sorry")
         console.log(response)
+      }
+    })
+  }
+
+  createNote(e) {
+    var ourNewPost = {
+      "title": $(".new-note-title").val(),
+      "content": $(".new-note-body").val(),
+      'status': 'publish'
+    }
+
+    $.ajax({
+      beforeSend: xhr => {
+        xhr.setRequestHeader("X-WP-Nonce", universityData.nonce)
+      },
+      url: universityData.root_url + '/wp-json/wp/v2/note/',
+      type: "POST",
+      data: ourNewPost,
+      success: response => {
+        $(".new-note-title, .new-note-body").val('');
+        $('<li>Imagine real data here.</li>').prependTo("#my-notes").hide().slideDown();
+        console.log("Congrats");
+        console.log(response);
+      },
+      error: response => {
+        console.log("Sorry");
+        console.log(response);
       }
     })
   }
