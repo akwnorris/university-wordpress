8502423f187f05ba1f95f0c3ac2bf9e0282db1a3
Alex Norris <akwnorris@gmail.com>
2023-10-30 13:03:28 -0500

    test delete button

--- app/public/wp-content/themes/fictional-university-theme/src/modules/MyNotes.js
+++ app/public/wp-content/themes/fictional-university-theme/src/modules/MyNotes.js
@@ -11,7 +11,18 @@ class MyNotes {
 
     //Methods go here
     deleteNote() {
-        alert("testing");
+        $.ajax({
+            url: universityData.root_url + '/wp-json/wp/v2/note/91',
+            type: 'DELETE',
+            success: (response) => {
+                console.log("Congrats");
+                console.log(response);
+            },
+            error: (response) => {
+                console.log("Sorry");
+                console.log(response);
+            }
+        });
     }
 }
 
