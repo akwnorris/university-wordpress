7cc7fc3c2bf1d6ad57d96348612da006b9f55780
Alex Norris <akwnorris@gmail.com>
2023-10-29 21:25:02 -0500

    create content template files

--- app/public/wp-content/themes/fictional-university-theme/search.php
+++ app/public/wp-content/themes/fictional-university-theme/search.php
@@ -3,26 +3,19 @@
 get_header(); 
 pageBanner(array(
   'title' => 'Search Results',
-  'subtitle' => 'You searched for &ldquo;' . get_search_query() . '&rdquo;'
+  'subtitle' => 'You searched for &ldquo;' . get_search_query(false) . '&rdquo;'
 ));
 ?>
 
 <div class="container container--narrow page-section">
   <?php 
     while(have_posts()) {
-      the_post(); ?>
-      <div class="post-item">
-        <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-
-        <div class="metabox">
-          <p>Posted by <?php the_author_posts_link(); ?> on <?php the_time('n.j.y'); ?> in <?php echo get_the_category_list(', '); ?></p>
-        </div>
-
-        <div class="generic-content">
-          <?php the_excerpt(); ?>
-          <p><a class="btn btn--blue" href="<?php echo the_permalink(); ?>">Continue reading &raquo;</a></p>
-        </div>
-    </div>
+      the_post(); 
+      
+      get_template_part('template-parts/content', get_post_type());
+      
+      ?>
+      
   <?php  }
 
       echo paginate_links();
