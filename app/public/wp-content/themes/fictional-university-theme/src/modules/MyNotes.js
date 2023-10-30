87201a91d34514bcb0744cfadc770bcec2aa1f03
Alex Norris <akwnorris@gmail.com>
2023-10-30 13:29:31 -0500

    turn edit button to cancel when in edit mode

--- app/public/wp-content/themes/fictional-university-theme/src/modules/MyNotes.js
+++ app/public/wp-content/themes/fictional-university-theme/src/modules/MyNotes.js
@@ -13,6 +13,7 @@ class MyNotes {
     //Methods go here
     editNote(e) {
         var thisNote = $(e.target).parents("li");
+        thisNote.find(".edit-note").html('<i class="fa fa-times" aria-hidden="true"></i> Cancel');
         thisNote.find(".note-title-field, .note-body-field").removeAttr("readonly").addClass("note-active-field");
         thisNote.find(".update-note").addClass("update-note--visible");
     }
