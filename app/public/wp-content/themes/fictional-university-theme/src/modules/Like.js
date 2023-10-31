12871976c0c7a02be3ace05a6da1042ea9cd6a41
Alex Norris <akwnorris@gmail.com>
2023-10-30 22:24:58 -0500

    let users like content

--- app/public/wp-content/themes/fictional-university-theme/src/modules/Like.js
+++ app/public/wp-content/themes/fictional-university-theme/src/modules/Like.js
@@ -2,29 +2,48 @@ import $ from "jquery"
 
 class Like {
   constructor() {
-    this.events()
+    this.events();
   }
 
   events() {
-    $(".like-box").on("click", this.ourClickDispatcher.bind(this))
+    $(".like-box").on("click", this.ourClickDispatcher.bind(this));
   }
 
+  // methods
   ourClickDispatcher(e) {
-    var currentLikeBox = $(e.target).closest(".like-box")
+    var currentLikeBox = $(e.target).closest(".like-box");
 
     if (currentLikeBox.data("exists") == "yes") {
-      this.deleteLike()
+      this.deleteLike();
     } else {
-      this.createLike()
+      this.createLike();
     }
   }
 
   createLike() {
-    alert("create test message")
+    $.ajax({
+      url: universityData.root_url + "/wp-json/university/v1/manageLike",
+      type: "POST",
+      success: response => {
+        console.log(response)
+      },
+      error: response => {
+        console.log(response)
+      }
+    })
   }
 
   deleteLike() {
-    alert("delete test message")
+    $.ajax({
+      url: universityData.root_url + "/wp-json/university/v1/manageLike",
+      type: "DELETE",
+      success: response => {
+        console.log(response)
+      },
+      error: response => {
+        console.log(response)
+      }
+    })
   }
 }
 
