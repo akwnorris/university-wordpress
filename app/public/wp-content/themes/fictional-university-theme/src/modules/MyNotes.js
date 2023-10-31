5f7df515272e22c400d1e870bdc4fbeda435a535
Alex Norris <akwnorris@gmail.com>
2023-10-30 21:43:53 -0500

    allow users to create new notes after deleting

--- app/public/wp-content/themes/fictional-university-theme/src/modules/MyNotes.js
+++ app/public/wp-content/themes/fictional-university-theme/src/modules/MyNotes.js
@@ -49,6 +49,9 @@ class MyNotes {
         thisNote.slideUp()
         console.log("Congrats")
         console.log(response)
+        if (response.userNoteCount < 5) {
+            $(".note-limit-message").removeClass("active");
+        }
       },
       error: response => {
         console.log("Sorry")
